00bb63639a2b492a4b53937a94df0b51060a3790
Anybaker <gdnesso@gmail.com>
2024-11-13 21:58:31 +0800

    some math

---
+++ just learn/math/divide.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void forceDivide(int n)
+{
+    for(int i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            int s=0;
+            while(n%i==0)
+            {
+                n/=i;
+                s++;
+            }
+            std::cout << i << s << "times";
+        }
+    }
+}
+
+void divide(int n)
+{
+    for(int i=2;i<=n/i;i++)
+    { 
+        if(n%i==0){
+            int s=0;
+            while(n%i==0){
+                n/=i;
+                s++;
+            }
+            std::cout << i << s << "times";
+        }
+        
+    }
+    if(n > 1) std::cout << n << " " << 1;
+}
