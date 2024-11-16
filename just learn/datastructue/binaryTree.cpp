6168f430ef7589957cfaaba549b428379d2be412
sylfilinux <2301637429@qq.com>
2024-11-16 18:02:09 +0800

    DFS&BFS

--- just learn/datastructue/binaryTree.cpp
+++ just learn/datastructue/binaryTree.cpp
@@ -1,29 +1,108 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <queue>
 using namespace std;
 
-unordered_map<int,vector<int>> tree;
+unordered_map<int, vector<int>> tree;
 struct TreeNode
-{   
+{
     int val;
-    TreeNode* right;
-    TreeNode* left;
-    TreeNode(int _val): val(_val),right(nullptr),left(nullptr) {}
+    TreeNode *right;
+    TreeNode *left;
+    TreeNode(int _val) : val(_val), right(nullptr), left(nullptr) {}
 };
 
+void inorderTraverse(TreeNode *root)//
+{
+    int curdepth=0;int mindepth=__INT_MAX__;int maxdepth=__FLT32_MIN__;
+    if (root == nullptr)
+        return;
 
+    curdepth++;
+    if(root->left==nullptr && root->right==nullptr) curdepth=max(curdepth,maxdepth);
+    
+    inorderTraverse(root->left);
+    cout << root->val << " ";
+    inorderTraverse(root->right);
+}
+
+void preorderTraverse(TreeNode *root)//root left right
+{
+    if (root == nullptr)
+        return;
+
+    cout << root->val << " ";
+    preorderTraverse(root->left);
+
+    preorderTraverse(root->right);
+}
+
+void postorderTraverse(TreeNode *root)//left right root
+{
+    if (root == nullptr)
+        return;
+
+    postorderTraverse(root->left);
+
+    postorderTraverse(root->right);
+    cout << root->val << " ";
+}
+
+void levelTraverse(TreeNode* root)
+{
+    if(root==nullptr) return;
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        TreeNode* cur=q.front();
+        cout << cur->val;
+        q.pop();
+
+        if(cur->left!=nullptr) q.push(cur->left);
+        if(cur->right!=nullptr) q.push(cur->right);
+    }
+}
+
+void leverOrderTraverse)TreeNode* root)
+{
+    queue<TreeNode*> q;
+    q.push(root);
+    int depth = 1;
+
+    
+    while(!q.empty())
+    {
+        int size=q.size();
+        for(int i=0;i<size;i++)
+        {
+            TreeNode* cur = q.front();
+            cout << "depth:" << depth << "which" << i << "value:" << cur->val;
+            q.pop;
+            i++
+
+            if(cur->left==nullptr&&cur->right==nullptr) return depth;
+
+            if(cur->left!=nullptr) q.push(q->left);
+            if(cur->right!=nullptr) q.push(q->right);
+        }
+        
+    depth++;
+    }
+}
 int main()
 {
-    TreeNode* root = new TreeNode(1);
+    TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
-    root->left->left =new TreeNode(4);
+    root->left->left = new TreeNode(4);
     root->right = new TreeNode(5);
     root->right->left = new TreeNode(6);
     root->right->right = new TreeNode(7);
 
-    //hashtable to simulate binarytree
-    tree[1]={2,5};
-    tree[2]={4};
-    tree[3]={6,7};
+    // hashtable to simulate binarytree
+    tree[1] = {2, 5};
+    tree[2] = {4};
+    tree[3] = {6, 7};
 }
--- test.cpp
+++ test.cpp
@@ -3,25 +3,9 @@
 #include <ctime>
 #include <algorithm>
 using namespace std;
-int num = 0;
-void pai(int begin, int end,int data[]) {
-    int i;
-    if (begin == end)num++;
-    else {
-        for (i = begin;i <= end;i++) {
-            swap(data[begin], data[i]);
-            pai(begin + 1, end, data);
-            swap(data[begin], data[i]);
-        }
-    }
-}
+
 int main()
 {
-    int data[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13 };
-    clock_t start, end;
-    start = clock();
-    pai(0, 11,data);
-    end = clock();
-    cout << (double)(end - start) / CLOCKS_PER_SEC <<"s" << endl;
-    return 0;
+	string s = "123";
+	printf("%c",s+4);
 }
